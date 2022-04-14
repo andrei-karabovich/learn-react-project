10b2bb50479c3a4f79844d494f6c7530d60971d9
andrei-karabovich <andreikarabovich@Andreis-MacBook-Pro.local>
2022-04-14 15:53:06 +0200

    initial app version

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/f/f7/Vandebron_logo.png'></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <img src='https://media.istockphoto.com/photos/extra-wide-evening-panorama-of-business-miami-skyline-picture-id1058108750?b=1&k=20&m=1058108750&s=170667a&w=0&h=YzDzPZd4CXZe1-G_cjL2WIylKsvK2JjgS9MGdQ2uy-Q='></img>
+            <div>Description</div>
+            <div>
+                Posts
+                <div>New Post</div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+      </div>
+    );
+}
+
+export default Profile;
