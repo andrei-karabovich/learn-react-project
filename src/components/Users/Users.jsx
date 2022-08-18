4b8b9921eb1d6670e339c2028509cfb5a7f464d5
andrei-karabovich <andreikarabovich@Andreis-MacBook-Pro.local>
2022-08-18 11:35:03 +0200

    splitted presentational and container components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,30 +1,28 @@
-import * as axios from "axios";
 import React from "react";
 import styles from './Users.module.css';
 
-class Users extends React.Component {
-
-    componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then( (response) => {
-            this.props.onSetUsers(response.data.items);
-        });
+const Users = (props) => {
+    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    const pages = [];
+    
+    for (let i = 1; i<= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onSubscribeButtonClick = (evt) => {
-        const value = evt.currentTarget.id;
-        this.props.onFollowButtonClick(parseInt(value));
-    }
-
-    render () {
-        return <div> {
-            this.props.users?.map(u => <div key={u.id} className={styles.userElement}>
+    return <div>
+        <div>
+            {pages.map(p => <span key={p} className={p != props.currentPage ? styles.pageNumber : styles.pageNumberSelected} 
+                             onClick={ (e) => {props.onPageNumberClick(p)} }> {p}</span>)}
+        </div>
+        <div> {
+            props.users?.map(u => <div key={u.id} className={styles.userElement}>
                 <img src={u.photos.small != null ? u.photos.small : 'https://bit.ly/3zu5DNw'} className={styles.userAvatar}/>
                 {u.name}
-                <button id={u.id} onClick={this.onSubscribeButtonClick}>{u.followed ? 'Unfollow' : 'Follow'}</button>
+                <button id={u.id} onClick={props.onSubscribeButtonClick}>{u.followed ? 'Unfollow' : 'Follow'}</button>
             </div>
             )}
         </div>
-    }
+    </div>
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,4 +1,6 @@
-import Users from "./UsersF";
+import Users from "./Users";
+import React, { useEffect } from "react";
+import * as axios from "axios";
 import {connect} from "react-redux";
 import {inverseIsFollow, setUsers, setUsersTotalCount, setCurrentPage} from "../../redux/usersReducer";
 
@@ -29,6 +31,30 @@ let mapDispatchToProps = (dispatch) => {
     }
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+const UsersContainer = (props) => {
+    useEffect(() => {
+        const endPoint = `https://social-network.samuraijs.com/api/1.0/users?count=${props.pageSize}&page=${props.currentPage}`;
+        axios.get(endPoint).then( (response) => {
+            props.onSetUsers(response.data.items);
+            props.onSetTotalCount(90);
+        });
+    }, []);
 
-export default UsersContainer;
+
+    const onSubscribeButtonClick = (evt) => {
+        const value = evt.currentTarget.id;
+        props.onFollowButtonClick(parseInt(value));
+    }
+
+    const onPageNumberClick = (pageNumber) => {
+        props.onSetCurrentPage(pageNumber);
+        const endPoint = `https://social-network.samuraijs.com/api/1.0/users?count=${props.pageSize}&page=${pageNumber}`;
+        axios.get(endPoint).then( (response) => {
+            props.onSetUsers(response.data.items);
+        });
+    }
+
+    return <Users users={props.users} currentPage={props.currentPage} totalUsersCount={props.totalUsersCount} pageSize={props.pageSize} onPageNumberClick={onPageNumberClick} onSubscribeButtonClick={onSubscribeButtonClick}/>
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
